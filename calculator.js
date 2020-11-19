c0b3f2dd194192d9e254909f782c69445f7df335
NimaAB <nimabewrani98@gmail.com>
2020-11-19 20:34:50 +0100

    writing to asked class

--- calculator.js
+++ calculator.js
@@ -1,13 +1,27 @@
 
-import * as math from './utils.js'
+//import * as math from './utils.js'
 
 //output section: 
 const asked = document.querySelector('.asked');
+console.log(asked.textContent) //geting the content.
+const content = asked.textContent; //should be parsed to number.  
+
 //operation btns:
 const plus = document.querySelector('.add');
 const minus = document.querySelector('.sub');
 const mulitply = document.querySelector('.mulitply');
 const div = document.querySelector('.div');
-//result to output:
+//all btns: 
+const btns = document.querySelectorAll('.btn')
 
-
+//writnig to asked:
+let text = ""; 
+let numbers = []; 
+btns.forEach(btn => {
+    btn.addEventListener('click',()=>{
+        text += btn.textContent;
+        asked.innerHTML = `${text}`;
+        numbers = text.split('');
+        console.log(numbers);
+    });
+});
