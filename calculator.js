7d75100f87c2c0fd4d279591f18d4c20c92e9853
NimaAB <nimabewrani98@gmail.com>
2020-11-19 17:33:40 +0100

    implementing operational methodes + - / *

---
+++ calculator.js
@@ -0,0 +1,13 @@
+
+import * as math from './utils.js'
+
+//output section: 
+const asked = document.querySelector('.asked');
+//operation btns:
+const plus = document.querySelector('.add');
+const minus = document.querySelector('.sub');
+const mulitply = document.querySelector('.mulitply');
+const div = document.querySelector('.div');
+//result to output:
+
+
